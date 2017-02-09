24e1cb8d08002b03f9116acba956eb023ab4bc65
mbuenoferrer <mbuenoferrer@gmail.com>
2017-02-09 02:01:20 +0100

    Rename adapters package

--- app/src/main/java/com/mbuenoferrer/popularmovies/adapters/MovieListAdapter.java
+++ app/src/main/java/com/mbuenoferrer/popularmovies/adapters/MovieListAdapter.java
@@ -1,4 +1,4 @@
-package com.mbuenoferrer.popularmovies.viewadapters;
+package com.mbuenoferrer.popularmovies.adapters;
 
 import android.content.Context;
 import android.support.v7.widget.RecyclerView;
